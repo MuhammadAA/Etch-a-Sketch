e47694c002019de6bf40c8104d7e6394b5cc1dfc
Muhammad Ansary <84735386+MuhammadAA@users.noreply.github.com>
2022-07-03 23:04:36 -0700

    CreateGrid function and Style changes
    Finished the createGrid function which makes the grid based on the selected size (currently users cannot change this by interacting with the website)
    
    made some visual changes, removed the border , added a background, changed background color for buttons, made the buttons change color while hovering.
    
    Made the color of the squares in the grid change color based on whether the mouse is hovering over it. (currently does not maintain color after the mouse is off a particular square).

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const slider = document.getElementById("#slider");
+let size = 16; //slide.value
+
+window.onload = function(){
+    createGrid(size)
+};
+
 function createGrid(size){
+    const grid = document.querySelector("#Container");
+    grid.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
+    for(let i = 0; i<size*size; i++){
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("id","box")
+
+        
+
+        grid.appendChild(newDiv)
+    }
 }
+
+//let box = document.querySelector("#box");
+//box.addEventListener("hover", function(e) {
+  //  e.target.style.backgroundColor = "white";
+//});
